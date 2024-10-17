5c94ed28e0fc3dd360a147bce0dbc1a8b534eeef
Gerardo Tapia <GreSt46854FHVFFF@hotmail.com>
2024-10-17 04:14:28 -0500

    Agregar archivos de proyecto.

---
+++ VEP.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ericka
+{
+    partial class VEP
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VEP));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(228, 186);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseUp);
+            // 
+            // VEP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(233, 189);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "VEP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ VEP.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Ericka
+{
+    public partial class VEP : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+        private PictureBox pictureBox;
+        private NotifyIcon notifyIcon;
+
+        public VEP()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+            this.TransparencyKey = Color.Black;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            pictureBox = new PictureBox
+            {
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(pictureBox);
+
+            pictureBox.MouseDown += PictureBox_MouseDown;
+            pictureBox.MouseMove += PictureBox_MouseMove;
+            pictureBox.MouseUp += PictureBox_MouseUp;
+
+            this.Load += Form1_Load;
+            this.Resize += Form_Resize;
+
+            CrearNotifyIcon();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        //ingresar el nombre del gif previamente importado en la carpeta Resources desde las propiedades
+            pictureBox.Image = RMP.Properties.Resources.cristals;
+            Debug.WriteLine("Imagen cargada en Form1_Load");
+        }
+
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+                Debug.WriteLine($"MouseDown - Cursor: {dragCursorPoint}, Form: {dragFormPoint}");
+            }
+        }
+
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+                Debug.WriteLine($"MouseMove - Nueva ubicación: {this.Location}");
+            }
+        }
+
+        private void PictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+            Debug.WriteLine("MouseUp - Arrastre finalizado");
+        }
+
+        private void CrearNotifyIcon()
+        {
+            notifyIcon = new NotifyIcon
+            {
+                Icon = RMP.Properties.Resources.emblem,
+                Visible = true,
+                Text = "Widget"
+            };
+
+            ContextMenuStrip menu = new ContextMenuStrip();
+            ToolStripMenuItem salirItem = new ToolStripMenuItem("Salir");
+            salirItem.Click += SalirItem_Click;
+            menu.Items.Add(salirItem);
+
+            notifyIcon.ContextMenuStrip = menu;
+
+
+            notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+        }
+
+
+        private void SalirItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+    }
+}
